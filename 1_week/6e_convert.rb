f0b92a75f95ca67e1349ebf5ea25c2603b3cecb2
Michael Alston <maalston@mkiassoc.com>
2014-08-08 03:21:54 -0400

    Convert method with tests
    We also explore rescuing an exception for non-float(able) values given
    to the convert method.
    
    We do not discussion that very poorly chosen name of convert.

--- 1_week/6e_convert.rb
+++ 1_week/6e_convert.rb
@@ -1,40 +1,44 @@
 # convert.rb
 
 =begin
-Exercise6. Write a method called convert that takes one argument which is a 
-temperature in degrees Fahrenheit. This method should return the temperature in degrees 
-Celsius. To format the output to say 2 decimal places, we can use the Kernel's format 
-method. For example, if x = 45.5678 then format("%.2f", x) will return the string 45.57. 
+Exercise6. Write a method called convert that takes one argument which is a
+temperature in degrees Fahrenheit. This method should return the temperature in degrees
+Celsius. To format the output to say 2 decimal places, we can use the Kernel's format
+method. For example, if x = 45.5678 then format("%.2f", x) will return the string 45.57.
 Another way is to use the round function as follows: puts (x*100).round/100.0
 =end
 
 #  Formular:  Temp in Celsius = (5/9) * (Fahrenheit - 32)
 
-def convert fahrenheit 
- apply_fomula = ((5.00/9.00) * (fahrenheit.to_f - 32.00))
- printf("%5.2f degrees Fahrenheit equals %5.2f Celcius \n\n", fahrenheit, apply_fomula)
+=begin
+doctest: convert -40 should be -40
+>> convert -40
+=> -40
+doctest: convert 32 should be 0
+>> convert 32
+=> 0
+doctest: convert 212 should be 100
+>> convert 212
+=> 100
+=end
+def convert fahrenheit
+  (fahrenheit - 32.0) *  5 / 9
 end
 
-convert 51.30
-
-
-
-
-
-
-
+[-40, 0, 32, 98, 98.6, 100, 212, 'Hey Mom!'].each do |fahrenheit|
+  begin
+    printf("%5.2f degrees Fahrenheit equals %5.2f Celcius \n\n", fahrenheit, convert(fahrenheit))
+  rescue NoMethodError
+    puts "Hey, you can't use #{fahrenheit.inspect} as an argument to convert!"
+  end
+end
 
 =begin  Test/debug code
-def convert fahrenheit 
+def convert fahrenheit
  puts "\nfahrenheit = #{fahrenheit} \n\n"
- 
  apply_fomula = ((5.00/9.00) * (fahrenheit.to_f - 32.00))
- 
  puts "apply_fomula = #{apply_fomula} \n\n"
- 
  puts "apply_fomula_rounded = #{apply_fomula.round} \n\n"
- 
  printf("%5.2f degrees Fahrenheit equals %5.2f Celcius \n\n", fahrenheit, apply_fomula)
-
 end
 =end
